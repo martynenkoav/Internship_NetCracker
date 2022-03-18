920dc76d15a2e789f54a4aa9adaa5e72839d9611
Nastya <martynenkoav.18@mail.ru>
2022-03-18 14:07:07 +0300

    Initial commit

---
+++ src/main/java/com/example/attempt/AttemptApplication.java
@@ -0,0 +1,18 @@
+package com.example.attempt;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttemptApplication {
+
+    public static void main(String[] args) {
+        Flyway flyway = Flyway.configure().dataSource("jdbc:postgresql://localhost:5432/Attempt_db", "postgres", "mamapapa").load();
+        flyway.migrate();
+      //  SpringApplication.run(AttemptApplication.class, args);
+
+
+    }
+
+}
