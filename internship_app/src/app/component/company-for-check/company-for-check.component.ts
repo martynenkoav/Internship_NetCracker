71f3b3fc178c2846c52a978f81f3f9fe0df913fa
MercilessYuna <aryund@mail.ru>
2022-05-13 01:42:18 +0300

    deleted extra lines in company-for-check

--- internship_app/src/app/component/company-for-check/company-for-check.component.ts
+++ internship_app/src/app/component/company-for-check/company-for-check.component.ts
@@ -33,11 +33,11 @@ enum TAGS {
 
 export class CompanyForCheckComponent implements OnInit {
 
-  public internships: Internship[];
-  public viewInternships: Internship[];
+  internships: Internship[];
+  internshipsWithoutFilt: Internship[];
   public company: Company;
-  private companyId: number;
-  private filters: Map<string, string> = new Map<string, string>();
+
+  companyId: number;
 
   constructor(private companyService: CompanyService, private tokenStorageService: TokenStorageService,
               private activatedRoute: ActivatedRoute, private internshipService: InternshipService) {
@@ -48,29 +48,27 @@ export class CompanyForCheckComponent implements OnInit {
       this.companyId = params['id'];
     });
 
-    this.filters.set("name", "");
-    this.filters.set("tag", "");
-
     this.getCompany();
     this.loadInternships();
   }
 
   getCompany() {
     this.companyService.getCompanyById(this.companyId).subscribe(
-      (company) => {
+      (response) => {
         console.log('Getting correctly');
-        this.company = company;
+        this.company = response;
       },
       error => console.warn(error)
     )
   }
 
   loadInternships() {
+
     this.internshipService.getInternshipsByCompanyId(this.companyId).subscribe(
-      (internships) => {
+      (response) => {
         console.log('Getting correctly');
-        this.internships = internships;
-        this.viewInternships = internships;
+        this.internships = response;
+        this.internshipsWithoutFilt = response;
         this.internships.forEach(internship => {
           internship.tags = internship.tags.map(tag => TAGS[tag]);
         });
@@ -79,13 +77,8 @@ export class CompanyForCheckComponent implements OnInit {
     )
   }
 
-  filterList(event: any, filterName: string) {
-    this.filters.set(filterName, event.target.value.toLowerCase());
-    this.viewInternships = this.internships;
-    this.filters.forEach((value, key) => {
-      if (value !== "") {
-        this.viewInternships = this.viewInternships.filter(x => x[key].toLowerCase().includes(value));
-      }
-    })
+  filterList(event: any) {
+    console.log(event);
+    this.internships = this.internshipsWithoutFilt.filter(x => x.name.toLowerCase().includes(event.target.value.toLowerCase()));
   }
 }
