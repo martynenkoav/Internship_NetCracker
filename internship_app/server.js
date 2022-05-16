5c9fc629f6bc6d018e7d53cd753167a376dbc05e
Nastya <martynenkoav.18@mail.ru>
2022-05-16 18:45:21 +0300

    front

--- internship_app/server.js
+++ internship_app/server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const path = require('path');
 
@@ -12,3 +13,14 @@ app.get('/', (req, res) =>
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8081)
+*/
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
